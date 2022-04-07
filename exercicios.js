d308883fd00072e411ce8537a10917537a3345fe
cassio <cassiospcjunior@gmail.com>
2022-04-07 02:51:10 -0300

    fiz ate o exercicio 5

--- exercicios.js
+++ exercicios.js
@@ -13,35 +13,51 @@
 //modulo
     console.log(nun4 % nun1);
 //exercicio 2
-    const numer = 5;
-    const numer2 = 54;
-    if (numer > numer2) {
+    const nume = 5;
+    const nume2 = 54;
+    if (nume > nume2) {
         console.log("O primeiro numero é maior que o segundo.");   
-    } else if (numer2 > numer) {
+    } else if (nume2 > nume) {
         console.log("O segundo numero é maior que o primeiro.");
     } else {
         console.log("Os números são iguais.");
     }
 //exercicio 3
-    const numer3 = 10;
-    if (numer > numer2) && (numer2 > numer3) && (numer > numer3){
+    const a = 2;
+    const b = 4;
+    const c = 3;
+    if (a > b && b > c){
         console.log("A ordem do maior para o menor é:");
-        console.log(numer);
-        console.log(numer2);
-        console.log(numer3);
-    } else if (numer < numer2) && (numer2 < numer3) && (numer3 > numer){
+        console.log(a);
+        console.log(b);
+        console.log(c);
+    } else if (a < b && b < c){
         console.log("A ordem do maior para o menor é:");
-        console.log(numer3);
-        console.log(numer2);
-        console.log(numer);
-    } else if (numer2 > numer) && (numer2 > numer3) && (numer > numer3){
+        console.log(c);
+        console.log(b);
+        console.log(a);
+    } else if (b > a && b > c && a > c){
         console.log("A ordem do maior para o menor é:");
-        console.log(numer2);
-        console.log(numer);
-        console.log(numer3);
-    } else if (numer2 > numer3) && (numer3 > numer){
+        console.log(b);
+        console.log(a);
+        console.log(c);
+    } else if (b > c && c > a){
         console.log("A ordem do maior para o menor é:");
-        console.log(numer2);
-        console.log(numer3);
-        console.log(numer);
+        console.log(b);
+        console.log(c);
+        console.log(a);
+    } else {
+        console.log("sintaxe incorreta!")
+    }
+//exercicio 4
+    const exe4 = true;
+    console.log(!exe4);
+//exercicio 5
+    const ang1 = 30;
+    const ang2 = 60;
+    const ang3 = 90;
+    if (ang1 + ang2 + ang3 == 180){
+        console.log("true,é um triangulo!");
+    }else {
+        console.log("false,não é um triangulo!");
     }
