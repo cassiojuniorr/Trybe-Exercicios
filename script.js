2b3596be778a5743b637197738fc68c3468ccae0
cassio <cassiospcjunior@gmail.com>
2022-04-08 15:01:53 -0300

    parte 1 exercicios

---
+++ script.js
@@ -0,0 +1,14 @@
+//5
+const n = 9;
+let resultado = 100;
+for (let i = 0; i <= n; i += 1){
+    resultado -= i;
+}
+console.log(resultado);
+//6
+const s = 5;
+let result = 0;
+for (i = 0; i <= s; i++){
+    result += i
+}
+console.log(result);
