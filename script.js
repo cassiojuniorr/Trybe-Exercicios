6ca2e86f46f332562cec1d92ad197232876d7ce6
cassio <cassiospcjunior@gmail.com>
2022-04-08 01:12:41 -0300

    final exercicios

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ for ( index of numbers){
 //exercicio 2
 console.log("--exercicio 2-");
 let soma = 0;
-for (index = 0;index , index < numbers.length;index++){
+for (index = 0; index < numbers.length;index++){
     soma += numbers[index];
 }
 console.log(soma);
@@ -27,5 +27,34 @@ if(media > 20){
 }else {
     console.log("valor menor ou igual a 20");
 }
+
 //exercicio 5
 console.log("--exercicio 5-");
+let maior = 0;
+for (index = 0; index < numbers.length;index++){
+    if(maior <= numbers[index]){
+        maior = numbers[index];
+    }
+}
+console.log("O maior número é: " ,maior);
+
+//exercicio 6
+console.log("--exercicio 6-");
+let impar = 0;
+for (index = 0; index < numbers.length;index++){
+    if (numbers[index] %2 == 1){
+        impar++;
+    }
+}
+console.log("Números impares: " , impar);
+
+//exercicio 7
+console.log("--exercicio 7-");
+let menor = numbers[0];
+
+for (index = 1; index < numbers.length;index++){
+    if(menor > numbers[index]){
+        menor = numbers[index];
+    }
+}
+console.log(menor);
