fa2ec0d21e1d1b92ae3d911779e5ed2699ca2c6d
cassio <cassiospcjunior@gmail.com>
2022-04-08 16:04:31 -0300

    ate o exercicio 3

---
+++ part2.js
@@ -0,0 +1,38 @@
+//exercicio 1
+console.log("--exercicio 1-");
+let fatorial = 1;
+
+for (let index = 10; index > 0; index -= 1) {
+  fatorial *= index;
+}
+
+console.log(fatorial);
+
+//exercicio 2
+console.log("--exercicio 2-");
+let word = 'tryber';
+let rever = '';
+
+for (let index = 0; index < word.length; index += 1) {
+  rever += word[word.length - 1 - index];
+}
+console.log(rever);
+
+//exercicio 3
+console.log("--exercicio 3-");
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let pala = '';
+for (let cont = 0; cont < array.length; cont++) {
+  pala = array[0].length;
+  if (pala > array[cont].length) {
+    pala = array[cont].length;
+  }
+}
+for (let most = 0; most < array.length; most++) {
+  if (pala == array[most].length) {
+    console.log('A palavra',array[most],"tem", pala, "caracteres sendo a menor palavra.")
+  }
+}
+
+//exercicio 4
+console.log("--exercicio 4-");
