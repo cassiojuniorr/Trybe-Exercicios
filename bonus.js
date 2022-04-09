0dc931dc06881fc08a71cc65c5745d3fa049a997
cassio <cassiospcjunior@gmail.com>
2022-04-09 20:01:49 -0300

    ate o exercicio 3

--- bonus.js
+++ bonus.js
@@ -1,8 +1,8 @@
 //exercicio 1
 console.log('--exercicio 1-');
-let n= 5;
+let n = 5;
 let ast = '*****';
-for (let cont = 0;cont <= 4; cont++){
+for (let cont = 0;cont <= n-1; cont++){
     console.log(ast);
 }
 
@@ -15,3 +15,23 @@ for(let cont = 0; cont <= size; cont++){
     console.log(lin);
     lin = lin + teris;
 }
+
+//exercicio 3
+console.log('--exercicio 3-');
+let n2 = 5;
+let aste = '*';
+let line = '';
+let posi = n;
+
+for (let cont = 0; cont < n; cont += 1) {
+  for (let index = 0; index <= n; index += 1) {
+    if (index < posi) {
+      line = line + ' ';
+    } else {
+      line = line + aste;
+    }
+  }
+  console.log(line);
+  line = '';
+  posi -= 1;
+};
