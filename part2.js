38fc220e73ac7f91a256cabf32e4d0a875561d56
cassio <cassiospcjunior@gmail.com>
2022-04-09 17:43:12 -0300

    terminei

--- part2.js
+++ part2.js
@@ -36,18 +36,21 @@ for (let most = 0; most < array.length; most++) {
 
 //exercicio 4
 console.log("--exercicio 4-");
-primos= ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20',
-'21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41',
-'42','43','44','45','46','47','48','49','50']
-let mar = primos[0];
-let pri = [];
-for(let cont = 0;cont < primos.length; cont++){
-  if (primos[cont] / primos[cont] && primos[cont] /  1){
-    pri.push(primos[cont]);
-    console.log(pri);
-  }else if (pri > mar){
-    mar = pri[cont]
+
+let maior = 0;
+
+for (let cont = 0; cont <= 50; cont += 1) {
+  let isPrime = true;
+  for (let limit = 2; limit < cont; limit += 1) {
+    if (cont % limit === 0) {
+      isPrime = false;
+    }
+  }
+  if (isPrime) {
+    maior = cont;
   }
 }
-console.log('O maior número primo entre 0 e 50 é: ',mar);
+
+console.log('O maior número primo entre 0 e 50 é: ',maior);
+
 
