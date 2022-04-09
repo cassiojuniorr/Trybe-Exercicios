e2e62b026f271148f8d33386b6db23f926a918b3
cassio <cassiospcjunior@gmail.com>
2022-04-09 18:29:29 -0300

    ate bonus 2

---
+++ bonus.js
@@ -0,0 +1,17 @@
+//exercicio 1
+console.log('--exercicio 1-');
+let n= 5;
+let ast = '*****';
+for (let cont = 0;cont <= 4; cont++){
+    console.log(ast);
+}
+
+//exercicio 2
+console.log('--exercicio 2-');
+let teris = '*';
+let lin= '';
+let size = 5;
+for(let cont = 0; cont <= size; cont++){
+    console.log(lin);
+    lin = lin + teris;
+}
