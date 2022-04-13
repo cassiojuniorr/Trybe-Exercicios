4c3f5e6260fd50359550c6c1f20a6c057ff0688f
cassio <cassiospcjunior@gmail.com>
2022-04-13 16:07:31 -0300

    peparando exercicios

---
+++ script.js
@@ -0,0 +1,2 @@
+document.getElementsByTagName("p")[0].innerText = "Me vejo formado na Trybe e trabalhando";
+document.getElementsByClassName("main.main-content").innerText = background-color : rgb(76,164,109);
